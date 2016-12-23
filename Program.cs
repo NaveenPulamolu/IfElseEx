0869a57888fde0baf5c941e70c9be88e7457e74f
NaveenPulamolu <naveenp992@gmail.com>
2016-12-22 23:49:19 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Enter the age");
+            a = Convert.ToInt32(Console.ReadLine());
+            if(a < 13)
+            {
+                Console.WriteLine("Hello kid");
+            }
+            else if(a <= 19)
+            {
+                Console.WriteLine("You a teenager");
+            }
+            else if (a > 19 && a <= 40)
+            {
+                Console.WriteLine("Energetic youth");
+            }
+            else
+            {
+                Console.WriteLine("Hello, Senior");
+            }
+            Console.ReadLine();
+        }
+    }
+}
